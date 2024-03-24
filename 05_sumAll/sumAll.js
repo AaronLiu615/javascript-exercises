9fcb23583266ea91a65fb2bf85b8322275f03f2d
Aaron Liu <99209428+AaronLiu615@users.noreply.github.com>
2024-03-23 23:18:56 -0400

    finished excerise 1-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
+    let val,start,end;
+    val = 0;
+    if(val1 < 0 || val2 < 0 || Number.isInteger(val1) == false || Number.isInteger(val2) == false){
+        return "ERROR";
+    }
+    if (val1 < val2){
+        start = val1;
+        end = val2;
+    }
+    else{
+        start = val2;
+        end = val1;
+    }
 
+    for(let i = start; i <= end; i++){
+        val += i;
+    }
+    return val;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(val) {
+  val = val-32;
+  val = val * (5/9);
+  return Math.round(val * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(val) {
+  val = val * (9/5);
+  val += 32;
+  return Math.round(val * 10)/10;
 };
 
 // Do not edit below this line
